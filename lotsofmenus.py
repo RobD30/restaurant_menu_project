79ac2242898287747d0f3fcb67f70b80e38f6f64
Robert Dickerson <rdickeriii@gmail.com>
2017-08-29 12:19:04 -0700

    Refactor: The great refactoring.
    Maybe shit will actually work now.
    
    end_of_line

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -1,9 +1,7 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Restaurant, Base, MenuItem, User
+from database_setup import Restaurant, Base, MenuItem, User, engine
 
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
