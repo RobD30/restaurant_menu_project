0aa3a4b4299595f6c738e8b82ff821ea9cd1f64a
Robert Dickerson <rdickeriii@gmail.com>
2017-09-13 10:02:57 -0700

    Refactor: READ THIS
    Added all the current fixes from instructor.
    How can you possible expect a student to even remotely get it right
    when your code is so broken from the course that I have spent like
    an actual extra damn week thinking my code was fucked when the sources
    were the problem. Then I finally pull all the code to try and see what
    fixes there are, and there are so many no wonder my code was broken.
    
    end_of_line

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -13,7 +13,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objkects in the
+# database session object. Any change made against the objects in the
 # session won't be persisted into the database until you call
 # session.commit(). If you're not happy about the changes, you can
 # revert all of them back to the last commit by calling
